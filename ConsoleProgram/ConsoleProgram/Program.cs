29e06599e9441256c3f294afac86a9c65be63fc4
lk9t9_sektor <lk9t9_sektor@mail.ru>
2018-10-19 01:06:47 +0800

    Методы. Необязательные и именованные параметры. Передача параметров по ссылке и значению. Ну и ещё заглавие добавил ;)

--- ConsoleProgram/ConsoleProgram/Program.cs
+++ ConsoleProgram/ConsoleProgram/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleProgram                    //пространство имен 
     {
         static void Main(string[] args)     // точка входа, метод Main, static - статистический, void - ничего не возвращает (метод №0)
         {                                   // блок операторов {}
+            Console.Title = "Обучение";     // заголовок программы
         start:                              // метка start:
             Console.WriteLine("\t\t\tВыберите метод: \n" +
                 "1. Получение данных из кучи различных функций\n" +
@@ -22,7 +23,8 @@ namespace ConsoleProgram                    //пространство имен 
                 "8. Условные выражения и логические операции\n" +
                 "9. Условные конструкции\n" +
                 "10. Циклы for/do/while. Операторы continue/break\n" +
-                "11. Массивы, цикл foreach, операторы Length, GetUpperBound");
+                "11. Массивы, цикл foreach, операторы Length, GetUpperBound\n" +
+                "12. Методы. Необязательные и именованные параметры. Передача параметров по ссылке и значению\n");
         select:
             Console.WriteLine("_________________________________________________________________");
             Console.Write("Выход из программы: 0. Отображения списка методов: ?\nВведите номер метода: ");
@@ -37,7 +39,7 @@ namespace ConsoleProgram                    //пространство имен 
                     Environment.Exit(0);                       // закрытие программы и передача кода 0
                     break;
                 case "1":
-                    Console.WriteLine("_______________Получение данных из Bunch.cs____________________");
+                    Console.WriteLine("_______________Получение данных из Bunch.cs_____________________");
                     //Bunch bunch = new Bunch();
                     Console.WriteLine(new Bunch().OfThings());
                     goto select;
@@ -90,24 +92,100 @@ namespace ConsoleProgram                    //пространство имен 
                     Console.WriteLine("_______________Получение данных из Array.cs______________________");
                     Console.WriteLine(new Array().Action());
                     goto select;
+                case "12":
+                    Console.WriteLine("_______________Получение данных из Methods.cs_____________________");
+                    Method0_5();
+                    //Console.WriteLine(new Array().Action());
+                    goto select;
                 default:
                     Console.WriteLine("Неверный метод, попробуйте ещё раз!");
                     goto select;
             }
         }
 
-        static int First()              // метод 1
+        static void Method0_5()
         {
+            int mthd = Method1();
+            Console.WriteLine(Method1());
+            Method2();
+            Console.WriteLine(Method3());
+
+            Console.Write("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 3;
+            int result = Sum(a, b);
+            Console.WriteLine(result);
+            result = Sum(a, 150);
+            Console.WriteLine($"{a} + 150 = " + result);
+            result = Sum(a + b + 10, 18);           // "x = a + b + 10", "y = 18"
+            Console.WriteLine(result);
+
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Display(name, age);
+
+            //OptionalParams(a, b);
+            int prm = OptionalParams(a, b);
+            Console.WriteLine(prm);
+            prm = OptionalParams(a, b, 4);
+            Console.WriteLine(prm);
+
+            int z = 5;
+            int d = 6;
+            Console.WriteLine($"InititalVal z = {z}");
+            Console.WriteLine("\tПередача параметров по значению"); // копия переменной z
+            AdditionalVal(z, d);
+            Console.WriteLine($"After AdditinalVal z = {z}");
+
+            Console.WriteLine("\tПередача параметров по ссылке");   // а тут прямо в ячейку памяти с z = x
+            AdditionalRef(ref z, d);
+            Console.WriteLine($"After AdditinalRef z = {z}");
+        }
+
+        static int Method1()            // метод 1
+        {
+            Console.Write("Возвращение значения из метода");
             int a = 12;
             return a;                   // return - возвращает значение и выходит из метода, всё что будет ниже не сработает!
         }
 
-        static string Method3() => ("Сокращенная запись метода");
+        static void Method2() => Console.WriteLine("Сокращенная запись метода");
+
+        static string Method3 () => "Возвраешние значения (return) в сокращенной записи метода";    // return "текст";
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        // через данный метод можно отображать string и int, т.е. замена постоянному написанию Console.WriteLine
+        static void Display(string name, int age)
+        {
+            Console.WriteLine($"Name: {name}  Age: {age}");
+        }
+
+        static int OptionalParams(int a1, int b1, int c1 = 3, int d1 = 10)
+        {
+            return a1 - b1 * c1 + d1;
+        }
+
+        static void AdditionalVal(int x, int y)         // передача по значению
+        {
+            x = x + y;
+            Console.WriteLine($"AdditionalVal x ={x}");
+        }
+
+        static void AdditionalRef(ref int x, int y)     // передача по ссылке
+        {
+            x = x + y;
+            Console.WriteLine($"AdditionalRef x ={x}");
+        }
 
         public void All()
         {
-            Console.WriteLine(First());     // краткая запись вызова из метода
-            Console.ReadKey();              // чтобы консоль не сразу закрывалась
+            Console.WriteLine(Method1());       // краткая запись вызова из метода
+            Console.ReadKey();                  // чтобы консоль не сразу закрывалась
         }
     }
 }
