b2fa66e19b7a6f0e6a42dc12d2078dc5a22ea885
lk9t9_sektor <lk9t9_sektor@mail.ru>
2019-04-23 04:52:01 +0800

    Свойства { get; set; } и инкапсуляция. Небольшие правки для SelectionMenu

--- ConsoleProgram/ConsoleProgram/Program.cs
+++ ConsoleProgram/ConsoleProgram/Program.cs
@@ -1,15 +1,15 @@
 ﻿//namespace, пространства имен, из библиотек находящихся по адресу: C:\Windows\assembly
-using System;                               
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 //пространство имен ConsoleProgram
-namespace ConsoleProgram                    
+namespace ConsoleProgram
 {
     // класс Program в namespace ConsoleProgram (ConsoleProgram.Program)
-    class Program                           
+    class Program
     {
         // точка входа, метод Main, static - статистический, void - ничего не возвращает
         static void Main(string[] args)
@@ -21,173 +21,179 @@ namespace ConsoleProgram
             new Program().SelectionMenu();
 
             // Чтобы консоль не сразу закрывалась, в данном случае уже не нужно
-            Console.ReadKey();                  
+            Console.ReadKey();
         }
 
         private void SelectionMenu()
         {
+            string line = "__________________________";
             // метка start:
             start:
-                Console.WriteLine("\t\t\tВыберите метод: \n" +
-                    "1. Получение данных из кучи различных функций\n" +
-                    "2. Неявная типизация, получение типа переменной\n" +
-                    "3. Получение данных о переполнении переменной\n" +
-                    "4. Конвертация чисел из 10-ой системы счисления в 2, 8, 10, 16\n" +
-                    "5. Поразрядные операции\n" +
-                    "6. Операции присваивания\n" +
-                    "7. Преобразование базовых типов + try/catch\n" +
-                    "8. Условные выражения и логические операции\n" +
-                    "9. Условные конструкции\n" +
-                    "10. Циклы for/do/while. Операторы continue/break\n" +
-                    "11. Массивы, цикл foreach, операторы Length, GetUpperBound\n" +
-                    "12. Методы\n" +
-                    "13. Параметры методов. Необязательные и именованные параметры\n" +
-                    "14. Передача входных параметров по значению и ссылке (ref)\n" +
-                    "15. Выходные параметры (out)\n" +
-                    "16. Ключевое слово params. Массив параметров\n" +
-                    "17. Область видимости (контекст) переменных\n" +
-                    "18. Рекурсивные функции (Факториал, число Фибоначи)\n" +
-                    "19. Перечисления enum\n" +
-                    "20. Кортежи\n" +
-                    "21. ООП: классы, объекты, this, конструкторы, инициализаторы объектов\n" +
-                    "22. Структуры\n" +
-                    "23. Типы значений и ссылочные типы, стек(stek), куча(heap)\n" +
-                    "24. Объекты классов как параметры методов\n" +
-                    "25. Модификаторы доступа\n");
+            Console.WriteLine("\t\t\tВыберите метод: \n" +
+                "1. Получение данных из кучи различных функций\n" +
+                "2. Неявная типизация, получение типа переменной\n" +
+                "3. Получение данных о переполнении переменной\n" +
+                "4. Конвертация чисел из 10-ой системы счисления в 2, 8, 10, 16\n" +
+                "5. Поразрядные операции\n" +
+                "6. Операции присваивания\n" +
+                "7. Преобразование базовых типов + try/catch\n" +
+                "8. Условные выражения и логические операции\n" +
+                "9. Условные конструкции\n" +
+                "10. Циклы for/do/while. Операторы continue/break\n" +
+                "11. Массивы, цикл foreach, операторы Length, GetUpperBound\n" +
+                "12. Методы\n" +
+                "13. Параметры методов. Необязательные и именованные параметры\n" +
+                "14. Передача входных параметров по значению и ссылке (ref)\n" +
+                "15. Выходные параметры (out)\n" +
+                "16. Ключевое слово params. Массив параметров\n" +
+                "17. Область видимости (контекст) переменных\n" +
+                "18. Рекурсивные функции (Факториал, число Фибоначи)\n" +
+                "19. Перечисления enum\n" +
+                "20. Кортежи\n" +
+                "21. ООП: классы, объекты, this, конструкторы, инициализаторы объектов\n" +
+                "22. Структуры\n" +
+                "23. Типы значений и ссылочные типы, стек(stek), куча(heap)\n" +
+                "24. Объекты классов как параметры методов\n" +
+                "25. Модификаторы доступа\n" +
+                "26. Свойства { get; set; } и инкапсуляция\n");
             select:
-                Console.WriteLine("_________________________________________________________________");
-                Console.Write("Выход из программы: 0. Отображения списка методов: ?\nВведите номер метода: ");
-                //int selection = Convert.ToInt32(Console.ReadLine());
-                string selection = Console.ReadLine();
-                // очистка консоли
-                Console.Clear();
-                switch (selection)
-                {
-                    case "?":
-                        // переход к метке start:
-                        goto start;
-                    case "0":
-                        // закрытие программы и передача кода 0
-                        Environment.Exit(0);
-                        break;
-                    case "1":
-                        Console.WriteLine("_______________Получение данных из Bunch.cs_____________________");
-                        //Bunch bunch = new Bunch();
-                        Console.WriteLine(new Bunch().OfThings());
-                        goto select;
-                    case "2":
-                        Console.WriteLine("_______________Получение данных из Implicit.cs__________________");
-                        Console.WriteLine(new Implicit().Variable());
-                        goto select;
-                    case "3":
-                        Console.WriteLine("_______________Получение данных из Overflow.cs__________________");
-                        Overflow overflow = new Overflow();
-                        Console.WriteLine(overflow.Action());
-                        //int meth_2 = Overflow();
-                        //Console.WriteLine($"Из метода №2: {Overflow()}");
-                        //Console.WriteLine(meth_2);
-                        goto select;
-                    case "4":
-                        Console.WriteLine("_______________Получение данных из Convertator.cs_______________");
-                        // создаём экземпляр нашего класса
-                        Convertator convertator = new Convertator();
-                        // вызываем метод экземпляра класса и получаем ответ ()
-                        Console.WriteLine(new Convertator().Numbers());
-
-                        // Далее не обязательно
-                        //int z1;                                  // переменная для получения результата
-                        //z1 = Convertator.Numbers();               // переданное значение присваиваем в переменнуюю z
-                        //Console.WriteLine(z1.GetType());         // получение типа переменной
-                        goto select;
-                    case "5":
-                        Console.WriteLine("_______________Получение данных из BitHandling.cs__________________");
-                        Console.WriteLine(new BitHandling().Action());
-                        goto select;
-                    case "6":
-                        Console.WriteLine("_______________Получение данных из Assignment .cs_______________");
-                        Console.WriteLine(new Assignment().Action());
-                        goto select;
-                    case "7":
-                        Console.WriteLine("_______________Получение данных из Preobrazovanie.cs_____________");
-                        Console.WriteLine(new Conversion().Action());
-                        goto select;
-                    case "8":
-                        Console.WriteLine("_______________Получение данных из Conditional.cs___________________");
-                        Console.WriteLine(new Conditional().Action());
-                        goto select;
-                    case "9":
-                        Console.WriteLine("_______________Получение данных из ConditionalConstructions.cs______");
-                        new ConditionalConstructions().Action();
+            Console.WriteLine("_____________________________________________________________________");
+            Console.Write("Выход из программы: 0. Отображения списка методов: ?\nВведите номер метода: ");
+            //int selection = Convert.ToInt32(Console.ReadLine());
+            string selection = Console.ReadLine();
+            // очистка консоли
+            Console.Clear();
+            switch (selection)
+            {
+                case "?":
+                    // переход к метке start:
+                    goto start;
+                case "0":
+                    // закрытие программы и передача кода 0
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    Console.WriteLine($"{ line } Bunch { line }");
+                    //Bunch bunch = new Bunch();
+                    Console.WriteLine(new Bunch().OfThings());
                     goto select;
-                    case "10":
-                        Console.WriteLine("_______________Получение данных из Cycles.cs_____________________");
-                        Console.WriteLine(new Cycles().Action());
-                        goto select;
-                    case "11":
-                        Console.WriteLine("_______________Получение данных из Array.cs______________________");
-                        Console.WriteLine(new Array().Action());
-                        goto select;
-                    case "12":
-                        Console.WriteLine("_______________Получение данных из Methods.cs_____________________");
-                        new Methods().Method12();
-                        //Console.WriteLine(new Array().Action());
-                        goto select;
-                    case "13":
-                        Console.WriteLine("_______________Получение данных из Methods.cs_____________________");
-                        new Methods().Method13();
-                        goto select;
-                    case "14":
-                        Console.WriteLine("_______________Получение данных из Methods.cs_____________________");
-                        new Methods().Method14();
-                        goto select;
-                    case "15":
-                        Console.WriteLine("_______________Получение данных из Methods.cs_____________________");
-                        new Methods().Method15();
-                        goto select;
-                    case "16":
-                        Console.WriteLine("_______________Получение данных из Methods.cs_____________________");
-                        new Methods().Method16();
-                        goto select;
-                    case "17":
-                        Console.WriteLine("_______________Получение данных из Context.cs_____________________");
-                        new Context().Show();
-                        goto select;
-                    case "18":
-                        Console.WriteLine("______________Получение данных из Recursion.cs_____________________");
-                        new Recursion().Action();
-                        goto select;
-                    case "19":
-                        Console.WriteLine("______________Получение данных из Enumerable.cs____________________");
-                        new Enumerable().Action();
-                        goto select;
-                    case "20":
-                        Console.WriteLine("______________Получение данных из Tuples.cs________________________");
-                        new Tuples().Action();
-                        goto select;
-                    case "21":
-                        Console.WriteLine("______________Получение данных из Oop_classes_objects.cs____________");
-                        new Oop_classes_objects().Action();
-                        goto select;
-                    case "22":
-                        Console.WriteLine("______________Получение данных из Structures.cs______________________");
-                        new Structures().Action();
-                        goto select;
-                    case "23":
-                        Console.WriteLine("______________Получение данных из Types.cs___________________________");
-                        new Types().Action();
-                        goto select;
-                    case "24":
-                        Console.WriteLine("______________Получение данных из ObjectsLikeParameters.cs___________________________");
-                        new ObjectsLikeParameters().Action();
-                        goto select;
-                    case "25":
-                        Console.WriteLine("______________Получение данных из AccessModifiers.cs___________________________");
-                        new AccessModifiers().Action();
-                     goto select;
-                     default:
-                        Console.WriteLine("Неверный метод, попробуйте ещё раз!");
-                        goto select;
-                }
+                case "2":
+                    Console.WriteLine($"{ line } Implicit { line }");
+                    Console.WriteLine(new Implicit().Variable());
+                    goto select;
+                case "3":
+                    Console.WriteLine($"{ line } Overflow { line }");
+                    Overflow overflow = new Overflow();
+                    Console.WriteLine(overflow.Action());
+                    //int meth_2 = Overflow();
+                    //Console.WriteLine($"Из метода №2: {Overflow()}");
+                    //Console.WriteLine(meth_2);
+                    goto select;
+                case "4":
+                    Console.WriteLine($"{ line } Convertator { line }");
+                    // создаём экземпляр нашего класса
+                    Convertator convertator = new Convertator();
+                    // вызываем метод экземпляра класса и получаем ответ ()
+                    Console.WriteLine(new Convertator().Numbers());
+
+                    // Далее не обязательно
+                    //int z1;                                  // переменная для получения результата
+                    //z1 = Convertator.Numbers();               // переданное значение присваиваем в переменнуюю z
+                    //Console.WriteLine(z1.GetType());         // получение типа переменной
+                    goto select;
+                case "5":
+                    Console.WriteLine($"{ line } BitHandling { line }");
+                    Console.WriteLine(new BitHandling().Action());
+                    goto select;
+                case "6":
+                    Console.WriteLine($"{ line } Assignment { line }");
+                    Console.WriteLine(new Assignment().Action());
+                    goto select;
+                case "7":
+                    Console.WriteLine($"{ line } Conversion { line }");
+                    Console.WriteLine(new Conversion().Action());
+                    goto select;
+                case "8":
+                    Console.WriteLine($"{ line } Conditional { line }");
+                    new Conditional().Action();
+                    goto select;
+                case "9":
+                    Console.WriteLine($"{ line } ConditionalConstructions { line }");
+                    new ConditionalConstructions().Action();
+                    goto select;
+                case "10":
+                    Console.WriteLine($"{ line } Cycles { line }");
+                    Console.WriteLine(new Cycles().Action());
+                    goto select;
+                case "11":
+                    Console.WriteLine($"{ line } Array { line }");
+                    Console.WriteLine(new Array().Action());
+                    goto select;
+                case "12":
+                    Console.WriteLine($"{ line } Method12 { line }");
+                    new Methods().Method12();
+                    //Console.WriteLine(new Array().Action());
+                    goto select;
+                case "13":
+                    Console.WriteLine($"{ line } Method13 { line }");
+                    new Methods().Method13();
+                    goto select;
+                case "14":
+                    Console.WriteLine($"{ line } Method14 { line }");
+                    new Methods().Method14();
+                    goto select;
+                case "15":
+                    Console.WriteLine($"{ line } Method15 { line }");
+                    new Methods().Method15();
+                    goto select;
+                case "16":
+                    Console.WriteLine($"{ line } Method16 { line }");
+                    new Methods().Method16();
+                    goto select;
+                case "17":
+                    Console.WriteLine($"{ line } Context { line }");
+                    new Context().Show();
+                    goto select;
+                case "18":
+                    Console.WriteLine($"{ line } Recursion { line }");
+                    new Recursion().Action();
+                    goto select;
+                case "19":
+                    Console.WriteLine($"{ line } Enumerable { line }");
+                    new Enumerable().Action();
+                    goto select;
+                case "20":
+                    Console.WriteLine($"{ line } Tuples { line }");
+                    new Tuples().Action();
+                    goto select;
+                case "21":
+                    Console.WriteLine($"{ line } Oop_classes_objects { line }");
+                    new Oop_classes_objects().Action();
+                    goto select;
+                case "22":
+                    Console.WriteLine($"{ line } Structures { line }");
+                    new Structures().Action();
+                    goto select;
+                case "23":
+                    Console.WriteLine($"{ line } Types { line }");
+                    new Types().Action();
+                    goto select;
+                case "24":
+                    Console.WriteLine($"{ line } ObjectsLikeParameters { line }");
+                    new ObjectsLikeParameters().Action();
+                    goto select;
+                case "25":
+                    Console.WriteLine($"{ line } AccessModifiers { line }");
+                    new AccessModifiers().Action();
+                    goto select;
+                case "26":
+                    Console.WriteLine($"{ line } Properties_encapsulation { line }");
+                    new Properties_encapsulation().Action();
+                    goto select;
+                default:
+                    Console.WriteLine("Неверный метод, попробуйте ещё раз!");
+                    goto select;
+            }
         }
     }
 }
---
+++ ConsoleProgram/ConsoleProgram/Properties_encapsulation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProgram
+{
+    // 26. Свойства { get; set; } и инкапсуляция
+    class Properties_encapsulation
+    {
+        public void Action()
+        {
+            Person1 tom = new Person1();
+            tom.Name = "Tom";               // set, через value
+            tom.Age = 40;
+            string Name = tom.Name;         // get
+            int Age = tom.Age;
+            Console.WriteLine($"{ tom.Name}, {tom.Age}");    // get
+
+            tom.SurName = "Tomkins";
+            //Console.WriteLine($"{ tom.SurName}");    // get не установлен, нельзя
+            //tom.FullName = "Tom Tomkins"; // set не установлен, нельзя
+            Console.WriteLine(tom.FullName);
+            tom.Money = 3000;
+            Console.WriteLine($"Баланс = {tom.Money} \nДолг = {tom.Debt}");
+            //tom.Birth = 1979; // private set недоступен
+            Console.WriteLine(tom.Birth);
+            //tom.Patronymic = "Tomovich";  // необъявленный set - private
+        }
+    }
+    class Person1
+    {
+        private string name;
+        public string Name
+            // Стандартное определение свойства { get; set }
+            // Управляет доступом в переменной
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        // Свойство могут определять один блок, get или set
+        private string surName;
+        public string SurName
+        {
+            set { surName = value; }
+        }
+
+        public string FullName
+        {
+            get { return $"{name} {surName}"; }
+        }
+
+        // Пример инкапсуляции
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >=0 && value <99)
+                    age = value;
+            }
+        }
+
+        // Полное определение свойства. Авто-свойство
+        public int Money { get; set; }
+        // Присвоение значения авто-свойству
+        public int Debt { get; set; } = 560;
+
+        // Использование модификаторов доступа
+        private int birth = 1900;
+        public int Birth
+        {
+            get { return birth; }
+            // Невозможно будет установить дату извне класса
+            private set
+            {
+                birth = value;
+            }
+        }
+
+        // Можно передать значение через конструктор в классе
+        public Person1()
+        {
+            Birth = 1979;
+        }
+
+        // По умолчанию, не объявленное свойство будет - private
+        public string Patronymic { get; }
+
+        // Сокращенная запись свойств
+        private string empty;
+        // Эквивалентно public string Empty {get {return empty;} }
+        public string Empty => empty;
+    }
+}
